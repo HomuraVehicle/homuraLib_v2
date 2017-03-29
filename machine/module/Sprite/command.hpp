09932d61ee79a33984e7ec14dd04b8dfa2da7152
hmito <hmito@outlook.com>
2017-03-29 15:07:18 +0100

    Fix: sprite::command::getData have to be non-inline function

--- machine/module/Sprite/command.hpp
+++ machine/module/Sprite/command.hpp
@@ -145,26 +145,7 @@ namespace hmr{
 						}
 					};
 					//データを得るコマンド(初めのADDRESS(8の倍数)と、データ数指定)
-					inline command getData(unsigned int Address_, unsigned int PackSize_, ans_getData& Ans_getData){
-						unsigned char Str[16] = { 0x56, 0x00, 0x32, 0x0C, 0x00, 0x0A, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x0B };
-
-						//PackSizeは８の倍数なので、8の倍数にする！！　超えた分は読み捨てる方針で
-						if (PackSize_ % 8 != 0){
-							PackSize_ = PackSize_ + 8 - PackSize_ % 8;
-						}
-
-						command Command(id::GetData, Str, 16, 10 + PackSize_, Ans_getData);
-
-						//受信開始アドレス指定
-						Command.SendStr[8] = (unsigned char)(Address_ / 256);
-						Command.SendStr[9] = (unsigned char)(Address_ % 256);
-
-						//受信パケットのサイズ指定
-						Command.SendStr[12] = (unsigned char)(PackSize_ / 256);
-						Command.SendStr[13] = (unsigned char)(PackSize_ % 256);
-
-						return Command;
-					}
+					command getData(unsigned int Address_, unsigned int PackSize_, ans_getData& Ans_getData);
 					//圧縮率の指定(defo 0x36)
 					inline command setCompessRate(unsigned char CompressRate_){
 						unsigned char Str[9] = { 0x56, 0x00, 0x31, 0x05, 0x01, 0x01, 0x12, 0x04, 0x36 };
